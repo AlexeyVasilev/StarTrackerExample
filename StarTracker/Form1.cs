be14bdf2ef0358af3345e37894ef9dab2587c12d
AlexeyVasilev <vasilev.alexey@gmail.com>
2024-06-22 20:07:56 +0500

    initial commit

---
+++ StarTracker/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StarTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int xSize = 700;
+            int ySize = 700;
+            String fileToDisplay = "star_image_1.bmp";
+            Bitmap MyImage;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            MyImage = new Bitmap(fileToDisplay);
+            pictureBox1.ClientSize = new Size(xSize, ySize);
+            pictureBox1.Image = (Image)MyImage;
+        }
+    }
+}
